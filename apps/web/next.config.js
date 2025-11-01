085285208fad46f21692824fc1fc9713254b2c9b
codevoks <vanshkumar841@gmail.com>
2025-11-01 20:29:17 +0530

    seperate dotenv &  jose added

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,27 +1,27 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-import path from 'path';
-import fs from 'fs';
-import dotenv from 'dotenv';
-
-const envFilePath = path.resolve(process.cwd(), '../../.env');
-const envVars = dotenv.parse(fs.readFileSync(envFilePath));
-
-const publicEnv = {};
-for (const key in envVars) {
-  if (key.startsWith('NEXT_PUBLIC_')) {
-    publicEnv[key] = envVars[key];
-  }
-}
-
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    ...publicEnv,
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true };
 
 export default nextConfig;
+
+// import path from 'path';
+// import fs from 'fs';
+// import dotenv from 'dotenv';
+
+// const envFilePath = path.resolve(process.cwd(), '../../.env');
+// const envVars = dotenv.parse(fs.readFileSync(envFilePath));
+
+// const publicEnv = {};
+// for (const key in envVars) {
+//   if (key.startsWith('NEXT_PUBLIC_')) {
+//     publicEnv[key] = envVars[key];
+//   }
+// }
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   env: {
+//     ...publicEnv,
+//   },
+// };
+
+// export default nextConfig;
