ce4bddfce4b25947987366900a36b86e6a083387
codevoks <vanshkumar841@gmail.com>
2025-11-23 16:45:27 +0530

    queue's logs removed

--- packages/redis-utils/src/queue.ts
+++ packages/redis-utils/src/queue.ts
@@ -13,22 +13,11 @@ export async function consumeFromQueue(
   handler: (data: any) => Promise<void>
 ) {
   try {
-    // console.log("consumeFromQueue queueName " + queueName);
     while (true) {
-      //   console.log("BEFORE result");
       try {
-        // console.log("Queue length:", await queueConsumeClient.lLen(queueName));
-        // console.log(
-        //   "Queue contents:",
-        //   await queueConsumeClient.lRange(queueName, 0, -1)
-        // );
-        // const result = await queueConsumeClient.brPop(queueName, 0);
         const result = await queueConsumeClient.brPop(queueName, 5);
-        // console.log(" RESULT ", result);
-        // console.log("AFTER result");
         if (result?.element) {
           const data = JSON.parse(result.element);
-          // console.log("🔥 Got from queue " + queueName + " data -> ", data);
           await handler(data);
         }
       } catch (error) {
